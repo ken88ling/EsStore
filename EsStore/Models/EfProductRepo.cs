2dd448c8af6d3483bfcdb165c16b22dede34fe32
Ken Ling <ken88ling@gmail.com>
2018-02-20 01:42:47 +0800

    1. add Dbcontext 2. add appsetting.json 3. add configuration in startup 4. add UseDefaultServiceProvider in program.cs

---
+++ EsStore/Models/EfProductRepo.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using EsStore.Data;
+
+namespace EsStore.Models
+{
+    public class EfProductRepo : IProductRepository
+    {
+        private readonly ApplicationDbContext _context;
+
+        public EfProductRepo(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        public IQueryable<Product> Products => _context.Products;
+    }
+}
